93fbebc7383023f1d1834676354de63cb02354b1
Matheus Henrique Silva <matheus.hresilva@gmail.com>
2021-01-21 14:23:54 -0300

    Aula 9

---
+++ src/Itens.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Itens(props) {
+    return(
+        <p>{props.nome}</p>
+    )
+}
+
+export default Itens;
---
+++ src/ListaDeCompras.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Item from "./Itens";
+
+function ListaDeCompras(){
+    return (
+        <>
+            <Item nome="Batata" />
+            <Item nome="Arroz" />
+            <Item nome="Feijão" />
+            <Item nome="Milho" />
+            <Item nome="Melão" />
+        </>
+    )
+}
+
+export default ListaDeCompras;
